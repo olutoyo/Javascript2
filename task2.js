3a74fa455c26d864e780acebf2813902f3349639
olutoyo <olutoyo@gmail.com>
2022-06-25 23:45:04 +0200

    Task 2

---
+++ task2.js
@@ -0,0 +1,24 @@
+var operator = prompt("Please enter the operator you want to perform?");
+var number1 = prompt("Please enter the first number");
+var number2 = prompt("Please enter the second number");
+
+if (operator = "+") {
+  let result = number1 + number2
+} else if (operator == "-") {
+  let a = number1 - number2
+} else if (operator == "/") {
+  let result = number1 / number2
+} else if (operator == "*") {
+  let result = number1 * number2
+} else if (operator == "%") {
+  let result = number1 % number2
+} else {
+  console.log("Enter the correct operator");
+}
+
+alert('Your result is,  ' );
+
+
+
+
+
