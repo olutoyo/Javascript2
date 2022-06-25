46bff1d5cfbd0c170677fbc13c9dac54b8fabaee
olutoyo <olutoyo@gmail.com>
2022-06-26 01:46:13 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,24 +1,22 @@
-var operator = prompt("Please enter the operator you want to perform?");
-var number1 = prompt("Please enter the first number");
-var number2 = prompt("Please enter the second number");
+var operator = prompt("Please enter the operator you want to perform(e.g +,-,/,*,%)");
+//if (operator != '+' || '-' || '/' || '*' || '%' ) {
+    //alert('Enter a correct operator')
+//}
+let number1 = prompt("Please enter the first number");
+let number2 = prompt("Please enter the second number");
 
-if (operator = "+") {
-  let result = number1 + number2
+if (operator == "+") {
+  var result = +number1 + +number2
 } else if (operator == "-") {
-  let a = number1 - number2
+  var result = +number1 - +number2
 } else if (operator == "/") {
-  let result = number1 / number2
+  var result = +number1 / +number2
 } else if (operator == "*") {
-  let result = number1 * number2
+  var result = +number1 * +number2
 } else if (operator == "%") {
-  let result = number1 % number2
+  var result = +number1 % +number2
 } else {
-  console.log("Enter the correct operator");
+  alert("Enter the correct operator");
 }
 
-alert('Your result is,  ' );
-
-
-
-
-
+alert("Your result is  " + result);
